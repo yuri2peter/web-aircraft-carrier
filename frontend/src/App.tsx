78b100d2543e2f4849915024f79ee411bba713df
yuri2 <yuri2peter@qq.com>
2023-01-04 17:07:15 +0800

    feat: 使用arrow-frog状态管理

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './store';
+import { StoreProvider } from './store/state';
 import MainRoute from './routes';
 
 function App() {
   return (
-    <Provider store={store}>
+    <StoreProvider>
       <MainRoute />
-    </Provider>
+    </StoreProvider>
   );
 }
 
