f6bdc02efbbf6009b556a3dec27039b9501fad7f
yuri2 <yuri2peter@qq.com>
2023-01-05 10:12:50 +0800

    move env

--- server/constant.ts
+++ server/constant.ts
@@ -1,9 +1,9 @@
-import { loadEnvConfig } from "@next/env";
+import { loadEnvConfig } from '@next/env';
 
-export const IS_PROD = process.env.NODE_ENV === "production";
+export const IS_PROD = process.env.NODE_ENV === 'production';
 export const ROOT_PATH: string = __dirname;
 
-loadEnvConfig(".", !IS_PROD);
+loadEnvConfig('./env', !IS_PROD);
 
 export const { PORT } = process.env as unknown as {
   PORT: number;
