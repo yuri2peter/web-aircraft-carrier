6932715af9673741307879a476f15c165ac94109
yuri2 <yuri2peter@qq.com>
2023-01-10 16:44:48 +0800

    env path fix

--- server/appSettings.ts
+++ server/appSettings.ts
@@ -1,9 +1,10 @@
+import path from 'path';
 import { loadEnvConfig } from '@next/env';
 
 export const IS_PROD = process.env.NODE_ENV === 'production';
 export const ROOT_PATH: string = __dirname;
 
-loadEnvConfig('./env', !IS_PROD);
+loadEnvConfig(path.resolve(ROOT_PATH, 'env'), !IS_PROD);
 
 export const { PORT } = process.env as unknown as {
   PORT: number;
