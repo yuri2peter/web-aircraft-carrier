984bc2a57780b6534d6c2ce3ede49052366881cb
yuri2 <yuri2peter@qq.com>
2023-03-31 20:14:45 +0800

    精简服务端代码

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,15 @@
-import ora from 'ora';
-import { ExampleSchema } from '@local/common';
 import { startKoa } from './startKoa';
 import { startIO } from './startIO';
 import { initDb } from './db';
 import { PORT } from './configs';
 
-// ExampleSchema.parse({});
-
 async function main() {
-  envTest();
   await initDb();
   const server = startKoa();
   startIO(server);
 
   server.listen(PORT, () => {
     console.log('listening on port:' + PORT);
-    console.log(`http://localhost:${PORT}`);
   });
 }
 main();
-
-function envTest() {
-  // 用于测试纯esm模块的载入情况
-  const spinner = ora('Loading...').start();
-  spinner.succeed();
-}
