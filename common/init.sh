cbef1b45338e69f53ad13d693546fb367ab66b0b
yuri2 <yuri2peter@qq.com>
2023-04-10 23:32:26 +0800

    调整common安装逻辑

---
+++ common/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Installing common package..."
+npm ci
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+cd ../common
+sh init.sh
+
 cd ../server
 sh init.sh
 npm i ../common
 
 cd ../frontend
 sh init.sh
+npm i ../common
 
 echo "Done."
