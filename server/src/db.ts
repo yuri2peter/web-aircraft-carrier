3a965944f88f42f2a3f4578bb35b7ed84a98e9b7
yuri2 <yuri2peter@qq.com>
2023-08-04 11:50:38 +0800

    db支持自动备份

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,12 +3,18 @@ import { ROOT_PATH } from './configs';
 import JsonDb from './libs/jsonDb';
 
 const dbFile = path.resolve(ROOT_PATH, './data/db/main.json');
+const dbBackUpDir = path.resolve(ROOT_PATH, './data/db/main_backup');
 
 const defaultValue = { _restartCount: 0 };
 const version = 1;
 
 export const db = new JsonDb({
   file: dbFile,
+  backup: {
+    dir: dbBackUpDir,
+    cronExp: '*/30 * * * *',
+    maxBackups: 150,
+  },
   version,
   defaultValue,
   versionFixer: (value, oldVersion) => {
@@ -20,8 +26,8 @@ export const db = new JsonDb({
   },
 });
 
+// 初始化使对数据库进行的操作
 export async function initDb() {
-  // dev restart counter
   db.changeData((d) => {
     d._restartCount += 1;
   });
--- server/src/libs/jsonDb.ts
+++ server/src/libs/jsonDb.ts
@@ -2,16 +2,27 @@
  * A simple database based json file.
  * @author yuri2peter
  */
-
+import dayjs from 'dayjs';
+import { Cron } from 'croner';
 import { Immutable, produce } from 'immer';
 import fs from 'fs-extra';
 import lodash from 'lodash';
+import { z } from 'zod';
+import path from 'path';
 
 const { throttle } = lodash;
+
+const backupPlanSchema = z.object({
+  dir: z.string(), // 备份目录
+  maxBackups: z.number().int().min(1), // 备份文件最大数量
+  cronExp: z.string(), // 定时任务 cron 描述。如 "0 23 * * *" 表示每天23:00执行；"*/30 * * * *" 表示每隔30分钟执行一次。
+});
+type BackupPlan = z.infer<typeof backupPlanSchema>;
 
 export default class JsonDb<T> {
   readonly file: string = '';
   readonly version: number = 0;
+  readonly debug: boolean = false;
   private data: Immutable<T>;
   private autoSaveFile: () => void = () => {};
 
@@ -25,12 +36,14 @@ export default class JsonDb<T> {
    * @param autoSaveWaitMilliseconds throttle delay for auto-saving file
    */
   constructor(params: {
-    file: string;
-    defaultValue: T;
-    version?: number;
-    versionFixer?: (value: unknown, oldVersion: number) => T;
-    disableAutoSave?: boolean;
-    autoSaveWaitMilliseconds?: number;
+    file: string; // 数据文件
+    defaultValue: T; // 默认值
+    version?: number; // 版本号
+    versionFixer?: (value: unknown, oldVersion: number) => T; // 版本修复器
+    disableAutoSave?: boolean; // 是否禁用自动保存
+    autoSaveWaitMilliseconds?: number; // 自动保存延迟
+    backup?: BackupPlan;
+    debug?: boolean;
   }) {
     const {
       file,
@@ -39,7 +52,10 @@ export default class JsonDb<T> {
       versionFixer,
       disableAutoSave = false,
       autoSaveWaitMilliseconds = 8000,
+      backup,
+      debug,
     } = params;
+    this.debug = !!debug;
     this.file = file;
     this.version = version;
     this.data = defaultValue as Immutable<T>;
@@ -49,6 +65,7 @@ export default class JsonDb<T> {
       }, autoSaveWaitMilliseconds);
     }
     this.loadFile(versionFixer);
+    this.startBackupPlan(backup);
   }
 
   /**
@@ -91,6 +108,70 @@ export default class JsonDb<T> {
       console.log('[jsonDb] Error parsing db file, use default value.');
     }
     this.saveFile();
+  }
+
+  private startBackupPlan(backup?: BackupPlan) {
+    if (!backup) {
+      return;
+    }
+    backupPlanSchema.parse(backup);
+    const { dir, maxBackups, cronExp } = backup;
+    const job = Cron(
+      cronExp,
+      {
+        name: 'jsonDb_backup',
+      },
+      () => {
+        // 获取时间字符串
+        const timeStr = dayjs().format('YYYY-MM-DD HH:mm');
+        if (this.debug) {
+          console.log(`[jsonDb] Backup at ${timeStr}.`);
+        }
+
+        // 写入文件
+        const filePath = path.join(dir, `${timeStr}.json`);
+        fs.ensureFileSync(filePath);
+        fs.writeFileSync(
+          filePath,
+          JSON.stringify({
+            data: this.data,
+            version: this.version,
+            updatedAtString: new Date().toString(),
+            updatedAtTime: new Date().getTime(),
+          })
+        );
+
+        // 删除超限的备份
+        fs.ensureDirSync(dir);
+        const backupFileNames: string[] = [];
+        fs.readdirSync(dir).forEach((file) => {
+          if (file.endsWith('.json')) {
+            backupFileNames.push(file);
+          }
+        });
+        // 把文件名按时间排序（旧->新）
+        backupFileNames.sort((a, b) => {
+          const getDateStr = (s: string) => {
+            const reg = /^(.*)\.json$/;
+            const execRel = reg.exec(s);
+            return execRel ? execRel[1] : '';
+          };
+          return dayjs(getDateStr(a)).diff(dayjs(getDateStr(b)));
+        });
+        // 对超过上限的文件进行删除
+        for (
+          let index = 0;
+          index < backupFileNames.length - maxBackups;
+          index++
+        ) {
+          fs.unlink(path.join(dir, backupFileNames[index])).catch((e) => {
+            console.warn(
+              '[jsonDb] Error deleting backup file ' + backupFileNames[index]
+            );
+          });
+        }
+      }
+    );
   }
 
   saveFile() {
